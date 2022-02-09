299961b83c24bc8947a53e61392d7e923a5f314b
ml-michael <michael.zm@medialand.tw>
2022-02-09 12:19:46 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,5 @@ git init
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f git@work:MedialandDev/test-lexus.git master:gh-pages
+git push -f git@work:ml-michael/test-lexus.git master:gh-pages
 cd -
